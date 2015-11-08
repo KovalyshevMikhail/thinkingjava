277d999b80b548f022b3299703a737280c5afd6d
git_dev_home <kovalski2006@yandex.ru>
2015-11-08 21:04:17 +0300

    LBTJ-9 Выполнить упражнения главы 5 add (BaseExample) выполнил упражнение 4

---
+++ src/com/nebiroz/thinkingjava/lessons/chapter05/Exercise04.java
@@ -0,0 +1,56 @@
+package com.nebiroz.thinkingjava.lessons.chapter05;
+
+/**
+ * Created by Frost on 07.11.2015.
+ */
+
+import com.nebiroz.thinkingjava.lessons.BaseExample;
+
+public class Exercise04 extends BaseExample{
+    static int result = 0;
+
+    public Exercise04(){
+        header = "Exercise 04";
+        description = "Добавьте к классу из упражнения 3 перегруженный конструктор, принимающий в качестве параметра строка (String)<br/>"
+                + "и распечатывающий ее вместе с сообщением.";
+        codeExample = "class Test {\n"
+                + "	public Test() {\n"
+                + "     System.out.println(\"This is constructor of Test class!\");\n"
+                + "	}\n"
+                + "}\n"
+                + "	public Test(String str) {\n"
+                + "     System.out.println(str);\n"
+                + "     System.out.println(\"This is constructor of Test class!\");\n"
+                + "	}\n"
+                + "}\n"
+                + "...\n"
+                + "Test tst = new Test();\n"
+                + "Test tst2 = new Test(\"My string.\");\n"
+                + "\n"
+                + "Вывод строки после первого Test() должно быть - \"This is constructor of Test class!\"\n"
+                + "Вывод строки после второго Test(\"My string.\") должно быть - " +
+                "<br/>Сначала \"My string.\", потом \"This is constructor of Test class!\".";
+        printExampleText();
+    }
+
+    class Test{
+        public Test(){
+            printOutLn("This is constructor of Test class!");
+        }
+        public Test(String str){
+            printOutLn(str);
+            printOutLn("This is constructor of Test class!");
+        }
+    }
+
+    @Override
+    public String runExample() {
+        startOutResult();
+
+        Test tst = new Test();
+        Test tst2 = new Test("My string.");
+
+        stopOutResult();
+        return endExeceuteExample();
+    }
+}
